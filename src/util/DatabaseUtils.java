9f02b437d6bfbfb51baf1dbb0223d849fc1f81d3
Harshatha Senthilkumar <harshasenthill@gmail.com>
2025-08-09 16:20:28 +0530

    Delete src directory

--- src/util/DatabaseUtils.java
+++
@@ -1,9 +0,0 @@
-package util;
-import java.sql.*;
-
-public class DatabaseUtils {
-    public static Connection getConnection() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/appointments", "root", "password");
-    }
-}
